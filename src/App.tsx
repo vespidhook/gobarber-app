d8a4bdd2bda4e2a5dc183017b0120c1d07b93a73
Bruno Alves <vespidhook@gmail.com>
2020-07-24 12:45:01 -0300

    Configure Starting

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import codePush from 'react-native-code-push';
 import SplashScreen from 'react-native-splash-screen';
 
 import AppProvider from './hooks';
@@ -29,4 +30,6 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+})(App);
